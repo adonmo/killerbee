115d163aebfee034e8b99f8959c11edfc4ca87bc
Aditya Patwardhan <gamble09D@gmail.com>
2021-06-04 21:41:28 +0530

    Added a rudimentary test for Client creation helper

--- killerbee/src/test/java/com/adonmo/killerbee/helper/ConnectionHelperTest.kt
+++ killerbee/src/test/java/com/adonmo/killerbee/helper/ConnectionHelperTest.kt
@@ -1,13 +1,16 @@
 package com.adonmo.killerbee.helper
 
+import android.os.Handler
+import com.adonmo.killerbee.IMQTTConnectionCallback
 import com.adonmo.killerbee.adapter.ConnectOptions
+import io.mockk.MockKAnnotations
 import io.mockk.every
+import io.mockk.impl.annotations.MockK
 import io.mockk.mockk
-import org.eclipse.paho.client.mqttv3.MqttAsyncClient
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions
-import org.eclipse.paho.client.mqttv3.TimerPingSender
-import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Before
 import org.junit.Test
 import java.util.*
 import java.util.concurrent.ScheduledExecutorService
@@ -15,6 +18,22 @@ import javax.net.SocketFactory
 import javax.net.ssl.HostnameVerifier
 
 class ConnectionHelperTest {
+    @MockK(relaxed = true)
+    private lateinit var connectOptions: ConnectOptions
+
+    @MockK(relaxed = true)
+    private lateinit var mqttEventsHandler: Handler
+
+    @MockK(relaxed = true)
+    private lateinit var androidMqttActionCallback: IMQTTConnectionCallback
+
+    @MockK(relaxed = true)
+    private lateinit var executorService: ScheduledExecutorService
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this)
+    }
 
     @Test
     fun getMqttConnectOptions_WithoutWill() {
@@ -78,6 +97,19 @@ class ConnectionHelperTest {
         //TODO Better test with deep equality
         val mqttAsyncClient = ConnectionHelper.getMqttAsyncClient(connectOptions, executorService)
         assertEquals(mqttAsyncClient.serverURI, serverURI)
+    }
+
+    @Test
+    fun getMqttClientAdapter() {
+        every { connectOptions.serverURI } returns  "wss://hello.org/mqtt"
+        val client = ConnectionHelper.getMqttClientAdapter(
+            connectOptions,
+            mqttEventsHandler,
+            androidMqttActionCallback,
+            executorService
+        )
+
+        assertNotNull(client)
     }
 
 }
