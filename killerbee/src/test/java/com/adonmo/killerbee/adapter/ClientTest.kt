43b0e589a4b4a281e51abe65caf1b0468695460f
Aditya Patwardhan <gamble09D@gmail.com>
2021-06-04 16:46:28 +0530

    Client Tests 100%

--- killerbee/src/test/java/com/adonmo/killerbee/adapter/ClientTest.kt
+++ killerbee/src/test/java/com/adonmo/killerbee/adapter/ClientTest.kt
@@ -65,7 +65,7 @@ class ClientTest {
     }
 
     @Test
-    fun connect_isWorkingAsExpected() {
+    fun connect_TriggersCallbackOnError() {
         val exceptionOnConnect = Exception()
         every {
             mqttAsyncClient.connect(
@@ -75,7 +75,131 @@ class ClientTest {
             )
         } throws exceptionOnConnect
         client.connect()
-        verify { androidConnectionCallback.connectActionFinished(MQTTActionStatus.FAILED, connectOptions, exceptionOnConnect) }
+        verify(exactly = 1) { androidConnectionCallback.connectActionFinished(MQTTActionStatus.FAILED, connectOptions, exceptionOnConnect) }
     }
 
+    @Test
+    fun connect() {
+        client.connect()
+        verify(exactly = 0) { androidConnectionCallback.connectActionFinished(MQTTActionStatus.FAILED, connectOptions, any()) }
+    }
+
+    @Test
+    fun disconnect_TriggersCallbackOnError() {
+        val exceptionOnDisconnect = Exception()
+        every {
+            mqttAsyncClient.disconnect(
+                MQTTActionContext(action = MQTTAction.DISCONNECT),
+                mqttActionListener
+            )
+        } throws exceptionOnDisconnect
+        client.disconnect()
+        verify(exactly = 1) { androidConnectionCallback.disconnectActionFinished(MQTTActionStatus.FAILED, exceptionOnDisconnect) }
+    }
+
+    @Test
+    fun disconnect() {
+        client.disconnect()
+        verify(exactly = 0) { androidConnectionCallback.disconnectActionFinished(MQTTActionStatus.FAILED, any()) }
+    }
+
+    @Test
+    fun forceDisconnect_TriggersCallbackOnError() {
+        val exceptionOnDisconnect = Exception()
+        every {
+            mqttAsyncClient.disconnectForcibly()
+        } throws exceptionOnDisconnect
+        client.forceDisconnect()
+        verify(exactly = 1) { androidConnectionCallback.disconnectActionFinished(MQTTActionStatus.FAILED, exceptionOnDisconnect) }
+    }
+
+    @Test
+    fun forceDisconnect() {
+        client.forceDisconnect()
+        verify(exactly = 0) { androidConnectionCallback.disconnectActionFinished(MQTTActionStatus.FAILED, any()) }
+    }
+
+    @Test
+    fun publish_TriggersCallbackOnError() {
+        val exceptionOnPublish = Exception()
+        val topic = "ABC"
+        val payload = "DEF".toByteArray()
+        val qos = 0
+        val retained = false
+        every {
+            mqttAsyncClient.publish(
+                topic,
+                payload,
+                qos,
+                retained,
+                MQTTActionContext(
+                    action = MQTTAction.PUBLISH,
+                    messagePayload = payload
+                ),
+                mqttActionListener
+            )
+        } throws exceptionOnPublish
+        client.publish(topic, payload, qos, retained)
+        verify(exactly = 1) { androidConnectionCallback.publishActionFinished(MQTTActionStatus.FAILED, payload, exceptionOnPublish) }
+    }
+
+    @Test
+    fun publish() {
+        val topic = "ABC"
+        val payload = "DEF".toByteArray()
+        val qos = 0
+        val retained = false
+        client.publish(topic, payload, qos, retained)
+        verify(exactly = 0) { androidConnectionCallback.publishActionFinished(MQTTActionStatus.FAILED, any(), any()) }
+    }
+
+    @Test
+    fun subscribe_TriggersCallbackOnError() {
+        val exceptionOnSubscribe = Exception()
+        val topicFilter = "ABC"
+        val qos = 0
+        every {
+            mqttAsyncClient.subscribe(
+                topicFilter,
+                qos,
+                MQTTActionContext(action = MQTTAction.SUBSCRIBE, topic = topicFilter),
+                mqttActionListener
+            )
+        } throws exceptionOnSubscribe
+        client.subscribe(topicFilter, qos)
+        verify(exactly = 1) { androidConnectionCallback.subscribeActionFinished(MQTTActionStatus.FAILED, topicFilter, exceptionOnSubscribe) }
+    }
+
+    @Test
+    fun subscribe() {
+        val topicFilter = "ABC"
+        val qos = 0
+        client.subscribe(topicFilter, qos)
+        verify(exactly = 0) { androidConnectionCallback.subscribeActionFinished(MQTTActionStatus.FAILED, any(), any()) }
+    }
+
+    @Test
+    fun subscribeMultiple_TriggersCallbackOnError() {
+        val exceptionOnSubscribe = Exception()
+        val topicFilters = arrayOf("ABC", "DEF")
+        val qos = intArrayOf(1, 0)
+        every {
+            mqttAsyncClient.subscribe(
+                topicFilters,
+                qos,
+                MQTTActionContext(action = MQTTAction.SUBSCRIBE_MULTIPLE, topics = topicFilters),
+                mqttActionListener
+            )
+        } throws exceptionOnSubscribe
+        client.subscribeMultiple(topicFilters, qos)
+        verify(exactly = 1) { androidConnectionCallback.subscribeMultipleActionFinished(MQTTActionStatus.FAILED, topicFilters, exceptionOnSubscribe) }
+    }
+
+    @Test
+    fun subscribeMultiple() {
+        val topicFilters = arrayOf("ABC", "DEF")
+        val qos = intArrayOf(1, 0)
+        client.subscribeMultiple(topicFilters, qos)
+        verify(exactly = 0) { androidConnectionCallback.subscribeMultipleActionFinished(MQTTActionStatus.FAILED, any(), any()) }
+    }
 }
